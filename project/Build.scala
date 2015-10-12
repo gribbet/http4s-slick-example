0201bb080b855a72c8632a87530e063f28290062
gribbet <graham@airphrame.com>
2015-10-11 18:08:15 -0700

    Revolver

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,6 @@
 import sbt.Keys._
 import sbt._
+import spray.revolver.RevolverPlugin.Revolver
 
 object Build extends sbt.Build {
   lazy val project = Project(
@@ -8,4 +9,5 @@ object Build extends sbt.Build {
     .settings(
       version := "1.0.0",
       scalaVersion := "2.11.7")
+    .settings(Revolver.settings: _*)
 }
