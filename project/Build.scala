a9b4dbcf9c40ccc2a31c87c08c0ccdb383099aa9
gribbet <graham@airphrame.com>
2015-10-11 22:46:31 -0700

    Logging setup

--- project/Build.scala
+++ project/Build.scala
@@ -11,6 +11,8 @@ object Build extends sbt.Build {
       scalaVersion := "2.11.7",
       libraryDependencies ++= Seq(
         "com.typesafe.slick" %% "slick" % "3.0.3",
+        "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+        "org.slf4j" % "slf4j-simple" % "1.7.12",
         "com.h2database" % "h2" % "1.4.190"))
     .settings(Revolver.settings: _*)
 }
